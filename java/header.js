b6a79f9e3aa87f20d65c93f1683ef5fd5df260c2
platanob <bcarrasco2022@alu.uct.cl>
2022-09-28 14:58:07 -0300

    modo oscuro detalles

--- java/header.js
+++ java/header.js
@@ -3,8 +3,10 @@ function hola(){
     let elementStyle = window.getComputedStyle(element, 'hover');
     let elementColor = elementStyle.getPropertyValue('background-color');
     if(elementColor=="rgb(3, 23, 40)"){
-        document.body.style.backgroundColor(rgb(255,255,255))
+        document.body.style.backgroundColor="rgb(255,255,255)"
+        document.getElementById("modoos").textContent = "Modo Oscuro"
     }else{
-        document.body.style.backgroundColor(rgb(3, 23, 40))
+        document.body.style.backgroundColor="rgb(3, 23, 40)"
+        document.getElementById("modoos").textContent = "Modo Claro"
     }
 }
