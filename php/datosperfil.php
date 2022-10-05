277b04fb407a3b8ae45990ca98ba0532bd1a9924
platanob <bcarrasco2022@alu.uct.cl>
2022-10-05 16:04:25 -0300

    a

--- php/datosperfil.php
+++ php/datosperfil.php
@@ -1,5 +1,25 @@
 <?php
+echo "<h1>hola</h1>"
+$inc = include("conec.php")
+echo "<h1>hola</h1>"
+if($inc){
+    $consulta = "SELECT * FROM 'perfil-usuario'";
+    $resultado = mysqli_query($conex,$consulta);
+    if ($resultado){
+        while($row = $resultado->fetch_array()){
+            $id = $row['nombre']
+            ?>
+            <div>
+                <h3><?php echo $id?></h3>
+            </div>
+            <?php
+        }
+    }
+}else{
+    echo "no se pudo conectar"
+}
 
-echo <h1>hola</h1>
+
+echo "<h1>hola</h1>"
 
 ?>
