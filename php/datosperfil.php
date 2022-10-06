ae3f5d4785d0638b4be3a8695d072269f22dbfa2
platanob <bcarrasco2022@alu.uct.cl>
2022-10-05 21:50:17 -0400

    historial de usuario

--- php/datosperfil.php
+++ php/datosperfil.php
@@ -1,17 +1,26 @@
 <?php
 include_once("conec.php");
 if($conex){
-    $consulta = "SELECT * FROM perfil-usuario";
+    $consulta = "SELECT * FROM historial_usuario";
     $resultado = mysqli_query($conex,$consulta);
-    if ($resultado){
+    if ($resultado){?>
+        <TABLE BORDER>
+        <TR>
+        <TH>nombre-partida</TH> <TH>duracion</TH> <TH>id-partida</TH>
+        </TR>
+        <?php
         while($row = $resultado->fetch_array()){
-            $id = $row['nombre'];
+            $id = $row['id-partida'];
+            $nombre = $row['nombre-partida'];
+            $duracion = $row['duracion'];
             ?>
-            <div>
-                <h3><?php echo "id:".$id;?></h3>
-            </div>
+                    <TR>
+                        <TD><?php echo $nombre;?></TD> <TD><?php echo $duracion;?></TD> <TD><?php echo $id;?></TD>
+        </TR>
             <?php
-        }
+        }?>
+        </TABLE>
+        <?php
     }
 }else{
     echo "<h1>no se pudo conectar</h1>";
