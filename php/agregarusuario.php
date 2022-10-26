36a837591c12fb17fccc708a5b32144c2047cb6b
platanob <bcarrasco2022@alu.uct.cl>
2022-10-26 12:37:05 -0300

    agregar perfil (arreglado)

--- php/agregarusuario.php
+++ php/agregarusuario.php
@@ -1,7 +1,6 @@
 <?php
-
 //creamos conexion
-$conn = mysqli_connect("db.inf.uct.cl","A2020_nvalenzuela","A2022_nvalenzuela","A2022_nvalenzuela");
+$conn = mysqli_connect("db.inf.uct.cl","A2022_nvalenzuela","A2022_nvalenzuela","A2022_nvalenzuela");
 // Comprobamos conexion
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -9,7 +8,7 @@ if ($conn->connect_error) {
 //Recimos la informacion
 
 $nombre = $_POST['nombre'];
-$apellido = $_POST['apllido'];
+$apellido = $_POST['apellido'];
 $usuario = $_POST['usuario'];
 $correo = $_POST['correo'];
 $contra = $_POST['contrasena'];
@@ -17,8 +16,8 @@ $fecha = $_POST['fecha'];
 $carrera = $_POST['carrera'];
 echo $nombre;
 //Insertamos la informacion a la base de datos
-$sql = "INSERT INTO perfil_usuario(nombre,apellido,usuario,correo,fecha-registro,carrera,contrasena)
-    VALUES ('$nombre', '$apellido', '$usuario', $correo, '$fecha','$carrera','$contra)";
+$sql = "INSERT INTO perfil_usuario(nombre,apellido,usuario,correo,fecha_registro,carrera,contrasena)
+    VALUES ('$nombre','$apellido','$usuario','$correo','$fecha','$carrera','$contra')";
 $result = $conn->query($sql);
 
 ?>
